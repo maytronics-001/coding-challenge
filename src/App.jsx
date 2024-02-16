c01db59d9d9a493a6219dd598d2407532bbf75ac
Jeffrey France <jeffrey.france@maytronics.com>
2024-02-16 15:49:55 +0800

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { useState, useEffect } from "react"
+import Axios from "axios"
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetchDogs()
+  }, []);
+  const fetchDogs = () => {
+      const urlDogs = 'https://dog.ceo/api/breeds/image/random/20'
+      Axios.get(urlDogs)
+      .then(function (response) {
+        setData(response.data.message)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+      });
+  }
+  
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      {data.map((image) => {
+        <img src = {image}/>
+      })}
+    </>
   )
 }
 
