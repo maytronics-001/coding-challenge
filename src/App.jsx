e3283f37cfab7eb26ce45c1708d2631918074745
Jeffrey France <jeffrey.france@maytronics.com>
2024-02-16 15:02:15 +0800

    CODES DOG

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+
+  const [dogs, setDogs] = useState('');
+  const [button, setButton] = useState(false);
+
+  const ButtonData = () => {
+   fetch('https://dog.ceo/api/breeds/image/random/20', {
+      method : "GET"
+
+    })
+        .then(response => response.json())
+        .then(data => setDogs(data.message))
+  }
+
+  return ( 
+    <>
+      <button onClick={ButtonData}>FETCH DOG</button>
+
+      {dogs.map((dog, keyDog) => {
+        return <img key={keyDog} src={dog} />
+      })}
+    </>
+    // <h1 className="text-3xl font-bold underline">
+    //   Hello world!
+    // </h1>
   )
 }
 
