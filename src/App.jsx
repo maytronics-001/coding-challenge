f0c42d25a103e55dbf13c2a15c79bfb006d9f75c
Jeffrey France <jeffrey.france@maytronics.com>
2024-02-20 15:40:39 +0800

    done '

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react"
+import "./app.css"
 function App() {
+
+  const [dogs, setDogs] = useState([]);
+
+  async function logDogs() {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random/20");
+    const dogsResponse = await response.json();
+    console.log(dogsResponse);
+    setDogs(dogsResponse.message);
+
+  }
+  
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <button className="js-btn" onClick={logDogs}> Fetch Dogs! </button>
+      <div className="grid-container">
+        {
+          dogs.map((dog,index)=>{return <img className="grid-item" src={dog} key={index} alt="placeholder" />;})
+        }
+      </div>
+    </div>
+    
   )
+
+  
 }
+
+
+
+
+
 
 export default App
