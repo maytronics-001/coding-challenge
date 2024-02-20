4a952428fbf47a3bb50ce69fd1057844d1cdd9f5
Jeffrey France <jeffrey.france@maytronics.com>
2024-02-20 11:09:47 +0800

    jericho

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import './App.css'
+import { useState } from 'react'
+import axios from 'axios'
+
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  const [dogs,setDogs] = useState(null);
+
+  const handleClick = () => {
+    axios.get('https://dog.ceo/api/breeds/image/random/20').then(res => setDogs(res.data.message)).catch(err => console.log('err',err))
+  }
+
+  return (<div className='flexContainer'>
+    <button onClick={handleClick}>Click me</button>
+    <div className="gridContainer">
+      { dogs ? dogs.map((dog, index) => (<div key={index} className='gridItem'>
+        <img src={dog} alt="" />
+      </div>)) : <div>No dogs retrieved</div>}
+    </div>
+    </div>
   )
 }
 
